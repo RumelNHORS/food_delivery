fc920159e30fc83c01f8ff58dd48c1cad685c0ac
RumelNHORS <rumelnhors52@gmail.com>
2024-09-04 20:48:55 +0600

    Create Models for Order and Order Item.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
+PAYMENT_CHOICES = [
+        ('card', 'Card'),
+        ('cash', 'Cash'),
+    ]
 
 
 # CustomUser extends the default Django user model to include roles
@@ -42,3 +49,25 @@ class Modifier(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+# Models for Order
+class Order(models.Model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='orders', on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, related_name='orders', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_CHOICES)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.username}"
+
+# Models For Order Item
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, related_name='order_items', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name} (Order {self.order.id})"
